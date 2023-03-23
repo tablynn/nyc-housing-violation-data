14ad23f8536276e13182d6472ea38925fe9700fe
Tabitha <tabitha_lynn@brown.edu>
2023-03-22 23:26:51 -0400

    feat: merged on all spas

--- zip_code.py
+++ zip_code.py
@@ -21,26 +21,10 @@ with open('data/zipcode_demographics.json', 'w') as outfile:
     json.dump(scrape(ZIPCODE_DEMOGRAPHICS), outfile, indent=4)
 
 
-# dict= json.loads('data/all_spas.json')
-# df1 = json_normalize(dict)
-# dict2 = json.loads("data/zipcode_demographics.json")
-# df2 = json_normalize(dict2)
-#df1 = json_normalize('all_spas.json')
-#df2 = json_normalize('zipcode_demographics.json')   
 df1 = pd.read_json('data/all_spas.json')
 df2 = pd.read_json('data/zipcode_demographics.json')
 zipcode_merge_df = pd.merge(df1, df2, on='jurisdiction_name', how='left')
 print(zipcode_merge_df)
 # for col in zipcode_merge_df.columns:
 #     print(col)
-
-zipcode_json = zipcode_merge_df.to_json()
-#zipcode_clean = json.dumps(zipcode_json, indent=4)
-
-# with open('data/merged_zipcodes.json', 'w') as outfile:
-#     outfile.write(zipcode_clean)
-
 zipcode_merge_df.to_json(r'data/merged_zipcodes.json')
-
-# with open('data/merged_zipcodes.json', 'w') as outfile:
-#     json.dump(zipcode_json, outfile, indent=4)
